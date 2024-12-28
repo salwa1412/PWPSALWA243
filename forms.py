70f85944665fe555a59062424a4dccdb61f3393d
salwa1412 <salwafharah233@gmail.com>
2024-12-28 20:37:45 +0700

    update

--- forms.py
+++ forms.py
@@ -1,22 +1,29 @@
+# forms.py
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, SelectField
-from wtforms.validators import DataRequired, Email, EqualTo
+from wtforms import StringField, PasswordField, SubmitField, EmailField, SelectField
+from wtforms.validators import DataRequired, Email, Length
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    role = SelectField('Role', choices=[('user', 'User '), ('admin', 'Admin')], validators=[DataRequired()])
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=150)])
+    email = EmailField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    role = SelectField('Role', choices=[('user', 'User '), ('admin', 'Admin')], validators=[DataRequired()])
     submit = SubmitField('Register')
 
 class LoginForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(), Email()])
+    email = EmailField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Login')
 
 class EditUserForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired(), Email()])
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=150)])
+    email = EmailField('Email', validators=[DataRequired(), Email()])
     role = SelectField('Role', choices=[('user', 'User '), ('admin', 'Admin')], validators=[DataRequired()])
     submit = SubmitField('Update User')
+
+class AddUserForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=150)])
+    email = EmailField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    role = SelectField('Role', choices=[('user', 'User '), ('admin', 'Admin')], validators=[DataRequired()])
+    submit = SubmitField('Add User')
